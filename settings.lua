84ad0abf402d253d89acc0f6e47ccd3c6c974d09
Wiwiweb <wiwiweb2@gmail.com>
2024-10-11 10:44:49 -0700

    Add settings

--- settings.lua
+++ settings.lua
@@ -1,9 +1,43 @@
--- {
---   type = "bool-setting",
---   name = "",
---   setting_type = "startup",
---   order = "a",
---   default_value = false,
--- }
+local locations = {
+  {
+    name = "space",
+    localised_name = {"item-group-name.space"}
+  },
+  {
+    name = "vulcanus",
+    localised_name = {"space-location-name.vulcanus"}
+  },
+  {
+    name = "fulgora",
+    localised_name = {"space-location-name.fulgora"}
+  },
+  {
+    name = "gleba",
+    localised_name = {"space-location-name.gleba"}
+  },
+  {
+    name = "aquilo",
+    localised_name = {"space-location-name.aquilo"}
+  },
+  {
+    name = "endgame",
+    localised_name = {"mod-setting-name.hsas-endgame"}
+  },
+}
 
--- data:extend(quality_settings)
+local settings = {}
+for i, location in pairs(locations) do
+  table.insert(settings,
+    {
+      type = "bool-setting",
+      name = "hsas-reveal-" .. location.name,
+      localised_name = {"mod-setting-name.hsas-reveal", location.localised_name},
+      setting_type = "startup",
+      order = tostring(i),
+      default_value = false,
+    }
+  )
+end
+
+
+data:extend(settings)
